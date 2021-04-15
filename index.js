0eb51a3c501f03896300430edd4033a0c09e8f31
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-04-15 18:40:44 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>YOUR-NAME is the champion</h1>';
+document.body.appendChild(newHeader);
